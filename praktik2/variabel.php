600c29fd444f252e62f2e4b098fca9a28d24b6b8
subhansub <subhansub472@gmail.com>
2023-03-01 09:43:45 +0700

    pertemuan1

---
+++ praktik2/variabel.php
@@ -0,0 +1,19 @@
+<?php
+
+    /*
+        Variabel sistem
+        Variabel build in php
+    */
+    echo $_SERVER["DOCUMENT_ROOT"];
+
+    /*
+        Variabel user
+        Variabel yang dibuat oleh user
+    */
+    echo '<br><br>';
+
+    $name = 'Rul';
+    $name = 'Akmal';
+
+    echo "Nama saya adalah $name";
+?>
